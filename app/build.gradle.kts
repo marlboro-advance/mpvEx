25f7815b15e25fe44b875ca1f58d5d14fc454f12
MarlboroAdvance <marlboroadvance@zohomail.in>
2025-11-10 18:29:11 +0530

    chore: simplify and clean up release workflows

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "app.marlboroadvance.mpvex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 10
-    versionName = "1.0.0"
+    versionCode = 11
+    versionName = "1.1.0"
 
     vectorDrawables {
       useSupportLibrary = true
