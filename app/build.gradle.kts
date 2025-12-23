906f3018650fa4d1dbde25a844b71efd481b04de
MarlboroAdvance <marlboroadvance@zohomail.in>
2025-12-23 14:47:32 +0530

    chore: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "app.marlboroadvance.mpvex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 11
-    versionName = "1.1.0"
+    versionCode = 12
+    versionName = "1.2.0"
 
     vectorDrawables {
       useSupportLibrary = true
