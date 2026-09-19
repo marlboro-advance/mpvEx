7158a02cf10d742d4df939c967b6cdac1764a8e9
marlborooAdvance <marlboroadvance@zohomail.in>
2026-09-20 00:35:04 +0530

    chore: bump version to 1.3.0 (130)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "app.marlboroadvance.mpvex"
     minSdk = 26
     targetSdk = 36
-    versionCode = 129
-    versionName = "1.2.9"
+    versionCode = 130
+    versionName = "1.3.0"
 
     vectorDrawables {
       useSupportLibrary = true
